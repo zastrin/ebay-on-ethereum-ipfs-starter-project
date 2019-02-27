75316a894639f741577e475a0d4df0ea9f7eeba9
Mahesh Murthy <mahesh@zastrin.com>
2019-02-26 19:11:13 -0500

    Starter template for truffle 5 + webpack box

---
+++ app/src/index.js
@@ -0,0 +1,49 @@
+import Web3 from "web3";
+import "./app.css";
+import ecommerceStoreArtifact from "../../build/contracts/EcommerceStore.json";
+
+const App = {
+  web3: null,
+  account: null,
+
+  start: async function() {
+    const { web3 } = this;
+
+    try {
+      // get contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = ecommerceStoreArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(
+        ecommerceStoreArtifact.abi,
+        deployedNetwork.address,
+      );
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+
+    } catch (error) {
+      console.error("Could not connect to contract or chain.");
+    }
+  }
+};
+
+window.App = App;
+
+window.addEventListener("load", function() {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn(
+      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
+    );
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+    );
+  }
+
+  App.start();
+});
